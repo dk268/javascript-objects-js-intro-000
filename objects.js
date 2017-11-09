89fc99d0655e3ac833722d378597a953f8a6934b
Daniel Kerr <lorddanp@aol.com>
2017-11-09 04:20:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {doINeed : "anActualExample"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle};
+  playlist[artistName] = songTitle};
   return playlist;
 }
 
