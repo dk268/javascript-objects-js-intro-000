519e0c7e4073b19ed88ad1ff5f1aa4b91ee7ad8d
Daniel Kerr <lorddanp@aol.com>
2017-11-09 04:08:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {"songName": "songTitle"};
 
-updatePlaylist(playlistObject, artistName, songTitle){
+function updatePlaylist(playlistObject, artistName, songTitle) {
   playlist.playlistObject= artistName: ${songTitle};
 }
 
-removeFromPlaylist(playlistObject, artistName){
+function removeFromPlaylist(playlistObject, artistName){
   delete playlist.artistName;
 }
