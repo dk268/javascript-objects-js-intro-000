1de98c58c6ef3ff484753e903ecc37dd503b4953
Daniel Kerr <lorddanp@aol.com>
2017-11-09 04:27:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,15 @@ var playlist = {doINeed : "anActualExample"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+  //why does playlist = {artistname : songTitle} fail?
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
+
+  //why does delete playlist.artistName not work?
+
+
   return playlist;
 }
